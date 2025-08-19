02a22a218ce444314257d568d920f18d83e755a6
Keshat <keshat2004@gmail.com>
2025-08-19 13:19:18 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ import random
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -60,3 +61,4 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
